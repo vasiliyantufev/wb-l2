6ee67e267771ae393daa029e3f244b61600da6db
vasiliyantufev <vasiliyantufev@gmail.com>
2022-09-07 15:52:51 +0300

    patterns

--- tasks/1/main.go
+++ tasks/1/main.go
@@ -13,7 +13,7 @@ import (
 Программа должна быть оформлена как go module
 Программа должна корректно обрабатывать ошибки библиотеки: выводить их в STDERR и возвращать ненулевой код выхода в OS
 */
-
+ 
 func main() {
 	response, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
